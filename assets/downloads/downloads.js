88e62c5854f4be470d2dbc9cd70d63ce3affe81d
Rph-nsmb <Rph-nsmb@github.com>
2025-07-11 12:00:09 +0200

    Downloads page update

---
+++ assets/downloads/downloads.js
@@ -0,0 +1,50 @@
+const versionPaths = [
+
+  "/assets/downloads/latest.html",
+  "/assets/downloads/4.html",
+  "/assets/downloads/3.html",
+  "/assets/downloads/2.html",
+  "/assets/downloads/1.html",
+
+];
+
+const insertIds = [
+
+  "latest",
+  "v4",
+  "v3",
+  "v2",
+  "v1"
+  
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+	
+  for (let i = 0; i < insertIds.length; i++) {
+
+    const id = insertIds[i];
+    const versionPath = versionPaths[i];
+
+    const containerEl = document.getElementById(id);
+    if (!containerEl) {
+      console.error(`Can't find element with id « ${id} »`);
+      continue; // on poursuit malgré l'erreur
+    }
+
+    fetch(projectPath)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error loading ${versionPath} : ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(html => {
+        containerEl.innerHTML = html;
+      })
+      .catch(error => {
+        console.error("Fetch error :", error);
+        containerEl.textContent = `Le projet ${versionPath} n'a pas pu être chargé.`;
+      });
+  }
+
+});
